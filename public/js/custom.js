9a29dfa990f3cb65fdde0e54e4c605d0ff5a0368
Hameed Abdulrahaman <hameedabdulrahamann@gmail.comgit config --global user.name Hameed>
2019-11-26 09:24:42 +0100

    style page

--- public/js/custom.js
+++ public/js/custom.js
@@ -51,3 +51,20 @@ for (i = 0; i < col.length; i++) {
       }
     }
   }
+
+// modal
+let modal = document.querySelector('#myModal');
+let btn = document.querySelector("#myBtn");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
